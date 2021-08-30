664f1c34c5193f1e78f23c3ef6d5fc225b82b69a
Dragi996 <dragisamaslar@gmail.com>
2021-08-30 16:31:39 +0200

    Project

---
+++ JS/formsEng.js
@@ -0,0 +1,36 @@
+document.getElementById('submitBtn').addEventListener("click", validation);
+
+
+
+function validation(event) {
+  event.preventDefault();
+  var inputNameNode = document.getElementById('inputName');
+  var inputSurnameNode = document.getElementById('inputSurname');
+  var inputEmailNode = document.getElementById('inputEmail');
+  var commentNode = document.getElementById('comment'); 
+  var isCorrectValidationNode = document.getElementById('isCorrectValidation');
+  var allCommentsListNode = document.querySelector('.allComments');
+  console.log("kliknuto");
+  console.log(inputNameNode.value);
+  // console.log(inputSurnameNode);
+  // console.log(inputEmailNode);
+  // console.log(commentNode);
+  // console.log(isCorrectValidationNode);
+
+
+  if (!inputNameNode.value || !inputSurnameNode.value || !inputEmailNode.value
+    || !commentNode.value) {
+      isCorrectValidationNode.innerText="All fields must be filled!"
+  }else{
+    isCorrectValidationNode.innerText="";
+    var newLIElement=document.createElement('li');
+    newLIElement.innerHTML=`<div class="inlineDiv"><span class="fas fa-user"></span>
+  <h5>${inputNameNode.value} ${inputSurnameNode.value}</h5></div>
+  <p>${commentNode.value}</p>`
+    allCommentsListNode.appendChild(newLIElement);
+  }
+  inputNameNode.value="";
+  inputSurnameNode.value="";
+  inputEmailNode.value="";
+  commentNode.value="";
+}
